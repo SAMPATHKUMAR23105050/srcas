c69df9e0b270b27aae47e548c7820da6c3de4493
ASUS <ASUS@sampath-the-don>
2025-02-26 10:14:17 +0530

    first

--- sampath/src/sampath/java.java
+++ sampath/src/sampath/java.java
@@ -1,5 +1,22 @@
-package sampath;
+class Main {
+  public static void main(String[] args) {
+    
+    // declare variables
+    int a = 12, b = 5;
 
-public class java {
+    // addition operator
+    System.out.println("a + b = " + (a + b));
 
+    // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
+    // modulo operator
+    System.out.println("a % b = " + (a % b));
+  }
 }
